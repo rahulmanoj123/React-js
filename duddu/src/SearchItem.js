bea4c86e2e726c84bec507b11ada565096a1f565
Rahul <rahulmanoj123>
2024-07-30 12:09:34 +0530

    input

--- duddu/src/SearchItem.js
+++ duddu/src/SearchItem.js
@@ -12,6 +12,14 @@ const SearchItem = ({search, setSearch}) => {
         value={search}
         onChange={(e) => setSearch(e.target.value)}     
     />
+    <input 
+        id = 'search'
+        type="text" 
+        roll ='search'
+        placeholder='Search Items'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}     
+    />
     </form>
   )
 }
